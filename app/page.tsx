9eadbf6cc902ce6b9aa31efa2d30e4196662dd3f
Ian Price <ianpriceve@gmail.com>
2024-06-27 16:52:53 -0500

    optimizations. images, fonts, scripts, seo, lazy loading.

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,22 @@ import Link from 'next/link'
 import ProductCard from './components/ProductCard'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
+import bidoof from '@/public/images/bidoof.png'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
   return (
     <main>
-        <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+      {/* <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
-      <ProductCard />
+      <ProductCard /> */}
+      <Image
+        src={bidoof}
+        alt='logo'
+        className='object-contain'
+        sizes='100vw'
+      />
     </main>
   )
 }
